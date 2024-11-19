1fb2bc64a48a409a58253b91fbf055bc6fd00ad2
seoharu <yunseul@jbnu.ac.kr>
2024-11-20 05:49:32 +0900

    bugfix:languagefilter, sortfilter, ratingfilter 로직 수정

--- src/components/movieFilters/SortFilter.vue
+++ src/components/movieFilters/SortFilter.vue
@@ -1,37 +1,67 @@
+<!-- SortFilter.vue -->
 <template>
   <div class="filter-group">
     <label class="filter-label">정렬</label>
-    <select v-model="selectedSort" @change="handleSortChange" class="filter-select">
-      <option value="">기본</option>
-      <option v-for="option in sortOptions" :key="option.value" :value="option.value">
+    <select
+      :value="selectedSort"
+      @change="handleSortChange"
+      class="filter-select"
+    >
+      <option value="popularity.desc">기본</option>
+      <option
+        v-for="option in sortOptions"
+        :key="option.value"
+        :value="option.value"
+      >
         {{ option.label }}
       </option>
     </select>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+
+interface SortOption {
+  value: string;
+  label: string;
+}
+
+export default defineComponent({
   name: 'SortFilter',
-  data() {
-    return {
-      selectedSort: '',
-      sortOptions: [
-        { value: 'popularity.desc', label: '인기도 높은순' },
-        { value: 'popularity.asc', label: '인기도 낮은순' },
-        { value: 'vote_average.desc', label: '평점 높은순' },
-        { value: 'vote_average.asc', label: '평점 낮은순' },
-        { value: 'release_date.desc', label: '최신순' },
-        { value: 'release_date.asc', label: '오래된순' }
-      ]
+
+  props: {
+    selectedSort: {
+      type: String as PropType<string>,
+      default: 'popularity.desc'
     }
   },
-  methods: {
-    handleSortChange() {
-      this.$emit('change', this.selectedSort)
+
+  emits: {
+    change: (value: string) => true
+  },
+
+  setup(props, { emit }) {
+    const sortOptions: SortOption[] = [
+      { value: 'popularity.desc', label: '인기도 높은순' },
+      { value: 'popularity.asc', label: '인기도 낮은순' },
+      { value: 'vote_average.desc', label: '평점 높은순' },
+      { value: 'vote_average.asc', label: '평점 낮은순' },
+      { value: 'release_date.desc', label: '최신순' },
+      { value: 'release_date.asc', label: '오래된순' },
+    ]
+
+    const handleSortChange = (event: Event) => {
+      const target = event.target as HTMLSelectElement
+      emit('change', target.value)
+    }
+
+    return {
+      sortOptions,
+      handleSortChange
     }
   }
-}
+})
 </script>
 
 <style scoped>
