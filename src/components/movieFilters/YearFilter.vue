3160511c9992ab6f74a27d32331bc08e4eb51dbc
seoharu <yunseul@jbnu.ac.kr>
2024-11-20 05:52:26 +0900

    bugfix:yearfiltering 수정

--- src/components/movieFilters/YearFilter.vue
+++ src/components/movieFilters/YearFilter.vue
@@ -1,37 +1,73 @@
 <template>
   <div class="filter-group">
     <label class="filter-label">개봉년도</label>
-    <select v-model="selectedYear" @change="handleYearChange" class="filter-select">
+    <select
+      v-model="selectedYear"
+      @change="handleYearChange"
+      class="filter-select"
+    >
       <option value="">전체</option>
-      <option v-for="year in years" :key="year" :value="year">
-        {{ year }}년
-      </option>
+      <optgroup label="최근">
+        <option
+          v-for="year in recentYears"
+          :key="year"
+          :value="year"
+        >
+          {{ year }}년
+        </option>
+      </optgroup>
+      <optgroup label="과거">
+        <option
+          v-for="year in pastYears"
+          :key="year"
+          :value="year"
+        >
+          {{ year }}년
+        </option>
+      </optgroup>
     </select>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   name: 'YearFilter',
+
   data() {
+    const currentYear = new Date().getFullYear()
+    const recentYearCount = 10  // 최근 10년
+    const pastYearCount = 10    // 과거 10년
+
+    // 최근 연도 (현재 년도부터 내림차순)
+    const recentYears = Array.from(
+      { length: recentYearCount },
+      (_, i) => currentYear - i
+    )
+
+    // 과거 연도 (10년 단위로)
+    const pastYears = Array.from(
+      { length: pastYearCount },
+      (_, i) => currentYear - recentYearCount - (i * 10)
+    )
+
     return {
       selectedYear: '',
-      years: []
+      recentYears,
+      pastYears
     }
   },
-  created() {
-    const currentYear = new Date().getFullYear();
-    this.years = Array.from(
-      { length: currentYear - 1900 + 1 },
-      (_, i) => currentYear - i
-    );
-  },
+
+  emits: ['change'],
+
   methods: {
     handleYearChange() {
-      this.$emit('change', this.selectedYear)
+      const value = this.selectedYear === '' ? null : Number(this.selectedYear)
+      this.$emit('change', value)
     }
   }
-}
+})
 </script>
 
 <style scoped>
