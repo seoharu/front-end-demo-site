6877e17f3347988346cd6321697b7c317286165f
seoharu <yunseul@jbnu.ac.kr>
2024-11-26 01:36:17 +0900

    setting:deploy

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,8 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/front-end-demo-site/'
+    : '/'
 
 });
